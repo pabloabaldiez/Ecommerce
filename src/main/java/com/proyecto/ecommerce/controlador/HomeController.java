84f0f9665fad408670c05091e74debe21448390a
Pablo Abal Diez <112098403+pabloabaldiez@users.noreply.github.com>
2024-04-25 13:55:01 -0300

    home usuario con imagenes

---
+++ src/main/java/com/proyecto/ecommerce/controlador/HomeController.java
@@ -0,0 +1,26 @@
+package com.proyecto.ecommerce.controlador;
+
+import com.proyecto.ecommerce.repositorio.ProductoRespository;
+import com.proyecto.ecommerce.servicio.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping("")
+    public String home(Model model){
+
+        model.addAttribute("productos", productoService.findAll());
+
+        return "usuario/home";
+    }
+
+}
