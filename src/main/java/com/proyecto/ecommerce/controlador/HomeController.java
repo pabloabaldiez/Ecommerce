e55991576889baf671562e91e5a9d6d3b9638fac
Pablo Abal Diez <112098403+pabloabaldiez@users.noreply.github.com>
2024-04-29 11:41:49 -0300

    comenzando vista carrito

--- src/main/java/com/proyecto/ecommerce/controlador/HomeController.java
+++ src/main/java/com/proyecto/ecommerce/controlador/HomeController.java
@@ -1,5 +1,6 @@
 package com.proyecto.ecommerce.controlador;
 
+import com.proyecto.ecommerce.modelo.Producto;
 import com.proyecto.ecommerce.repositorio.ProductoRespository;
 import com.proyecto.ecommerce.servicio.ProductoService;
 import org.slf4j.Logger;
@@ -9,7 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -29,11 +33,20 @@ public class HomeController {
     }
 
     @GetMapping("/productohome/{id}")
-    public String productoHome(@PathVariable Integer id){
+    public String productoHome(@PathVariable Integer id, Model model){
 
-        LOOGER.info("Id enviado enviado como parametro {}", id);
+        Producto producto= new Producto();
+        Optional<Producto> productoOptional=productoService.getProducto(id);
+        producto=productoOptional.get();
+        model.addAttribute("producto", producto);
+
 
         return "usuario/productohome";
+    }
+
+    @PostMapping("/carrito")
+    public String addCarrito(){
+        return"usuario/carrito";
     }
 
 }
