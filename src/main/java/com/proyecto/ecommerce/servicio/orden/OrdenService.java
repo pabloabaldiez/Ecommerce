09fc328c4ec4dc3ad10f0c479b7304fbbeec6674
Pablo Abal Diez <112098403+pabloabaldiez@users.noreply.github.com>
2024-05-02 20:18:34 -0300

    contador ordenes

--- src/main/java/com/proyecto/ecommerce/servicio/orden/OrdenService.java
+++ src/main/java/com/proyecto/ecommerce/servicio/orden/OrdenService.java
@@ -2,8 +2,12 @@ package com.proyecto.ecommerce.servicio.orden;
 
 import com.proyecto.ecommerce.modelo.Orden;
 
+import java.util.List;
+
 public interface OrdenService {
 
     Orden save(Orden orden);
+
+    List<Orden> findAll();
 
 }
--- src/main/java/com/proyecto/ecommerce/servicio/orden/OrdenServicioImpl.java
+++ src/main/java/com/proyecto/ecommerce/servicio/orden/OrdenServicioImpl.java
@@ -5,6 +5,9 @@ import com.proyecto.ecommerce.repositorio.OrdenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class OrdenServicioImpl implements OrdenService{
 
@@ -15,4 +18,41 @@ public class OrdenServicioImpl implements OrdenService{
     public Orden save(Orden orden) {
         return ordenRepository.save(orden);
     }
+
+    @Override
+    public List<Orden> findAll() {
+        return ordenRepository.findAll();
+    }
+
+    public String generarNumeroOrden(){
+
+        int numero=0;
+        String numeroString="";
+
+       List<Orden> ordenes= findAll();
+       List<Integer> numeros=new ArrayList<Integer>();
+
+       ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+
+        if(ordenes.isEmpty()){
+
+            numero=1;
+
+        }else{
+
+            numero=numeros.stream().max(Integer::compare).get();
+            numero++;
+        }
+
+        if(numero<10){
+            numeroString="000000000"+String.valueOf(numero);
+        }else if(numero<100){
+            numeroString="00000000"+String.valueOf(numero);
+        } else if(numero<1000){
+            numeroString="0000000"+String.valueOf(numero);
+    }
+
+        return numeroString;
+    }
+
 }
