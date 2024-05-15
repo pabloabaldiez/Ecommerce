c15c60c803179bab6a391b98fcb19aee12a85b53
Pablo Abal Diez <112098403+pabloabaldiez@users.noreply.github.com>
2024-05-15 12:05:48 -0300

    seguridad

--- src/main/java/com/proyecto/ecommerce/servicio/seguridad/UserDetailServiceImpl.java
+++ src/main/java/com/proyecto/ecommerce/servicio/seguridad/UserDetailServiceImpl.java
@@ -22,30 +22,25 @@ public class UserDetailServiceImpl implements UserDetailsService{
     @Autowired
     private UsuarioService usuarioService;
     @Autowired
-    private BCryptPasswordEncoder bCrypt;
-
-    @Autowired
     HttpSession session;
-
     private Logger log = LoggerFactory.getLogger(UserDetailServiceImpl.class);
 
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
+        log.info("el username es:");
         Optional<Usuario> optionalUsu=usuarioService.findByEmail(username);
 
         if(optionalUsu.isPresent()){
+            log.info("esto es el id de lusuario {}:", optionalUsu.get().getId());
 
             session.setAttribute("idusuario", optionalUsu.get().getId());
             Usuario usuario=optionalUsu.get();
 
-            return User.builder().username(usuario.getNombre()).password(bCrypt.encode(usuario.getPassword())).rol(usuario.getRol()).build();
+            return User.builder().username(usuario.getNombre()).password(usuario.getPassword()).roles(usuario.getRol()).build();
 
         }else{
             throw new UsernameNotFoundException("Usuario no encontrado");
         }
-
-        return null;
     }
 }
